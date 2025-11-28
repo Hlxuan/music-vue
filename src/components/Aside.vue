12bc551629c379d6903779677f17bef23d794cee
Hlxuan <huanglangxuan@hlxuan.top>
2025-11-28 14:43:15 +0800

    update: 优化导航栏菜单

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- 导航菜单 -->
     <el-menu
       style="width: 250px; min-height: calc(100vh - 50px)"
       default-active="2"
@@ -12,39 +13,39 @@
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <span>用户模块</span>
         </template>
-
         <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="/consumer_list">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
+          <el-menu-item index="/consumer_list">用户列表</el-menu-item>
         </el-menu-item-group>
-
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
       </el-submenu>
-
-      <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-menu"></i>
+          <span>歌手模块</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="/singer_list">歌手列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-document"></i>
+          <span>歌单模块</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="/songList_list">歌单列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+      <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-setting"></i>
+          <span>歌曲模块</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="/song_list">歌曲列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
     </el-menu>
   </div>
 </template>
